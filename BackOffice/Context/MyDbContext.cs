3b646ca49b37d363851fd8ae4fe7e32e14cec7c4
Haroavo <razakamahefaharoavo9@gmail.com>
2025-12-29 21:41:39 +0300

    [feature/schedule] add schedule for user

--- BackOffice/Context/MyDbContext.cs
+++ BackOffice/Context/MyDbContext.cs
@@ -19,6 +19,8 @@ public partial class MyDbContext : DbContext
     public DbSet<SoldeConge> SoldeConges { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Registration> Registrations { get; set; }
+    
+    public DbSet<Schedule> Schedules { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ BackOffice/Entities/20251229183921_AddSchedulesTable.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackOffice.Entities
+{
+    /// <inheritdoc />
+    public partial class AddSchedulesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Day = table.Column<int>(type: "int", nullable: false),
+                    Start = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    End = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Working = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Schedules_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_UserId",
+                table: "Schedules",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
