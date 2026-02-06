35fb5ce758fb0b8f4fd9dd6622ff0ca8ecc45326
iaritina <iaritina7@gmail.com>
2026-02-06 10:11:38 +0300

    check if auth is valid

---
+++ FrontOffice/Auth/JwtAuthorizeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FrontOffice.Auth;
+
+public class JwtAuthorizeAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var isAuthenticated = context.HttpContext.User.Identity?.IsAuthenticated ?? false;
+
+        if (!isAuthenticated)
+        {
+            context.Result = new RedirectToActionResult("Login", "Auth", null);
+            return;
+        }
+
+        base.OnActionExecuting(context);
+    }
+}
