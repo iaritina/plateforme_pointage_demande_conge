83ba7a88f952521d3a395500063c02e0f3fe0553
iaritina <iaritina7@gmail.com>
2026-02-06 13:44:48 +0300

    fix login redirection

--- FrontOffice/Controllers/HomeController.cs
+++ FrontOffice/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using FrontOffice.Auth;
 using Microsoft.AspNetCore.Mvc;
 using FrontOffice.Models;
 
@@ -13,6 +14,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [JwtAuthorize]
     public IActionResult Index()
     {
         return View();
